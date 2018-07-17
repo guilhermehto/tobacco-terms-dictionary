79988ef0340bc75678e743a9da7350562fea05cc
Eduardo Godoy <eduardo.ewgo@gmail.com>
2018-07-17 19:59:21 -0300

    Couldn't find any 'like' operator to query in firestore, so I had to use a workaround.

--- src/pages/terms/terms.ts
+++ src/pages/terms/terms.ts
@@ -3,6 +3,7 @@ import {NavController, NavParams} from 'ionic-angular';
 import {Observable} from 'rxjs';
 import {TermsProvider} from "../../providers/terms/terms";
 import {Term} from "../../interfaces/term";
+import {AlertCtrlProvider} from "../../providers/alert-ctrl/alert-ctrl";
 
 @Component({
     selector: 'page-home',
@@ -10,19 +11,31 @@ import {Term} from "../../interfaces/term";
 })
 export class TermsPage {
 
-    terms: Observable<Term[]>;
+    terms$: Observable<Term[]>;
+    terms: Term[];
     search: string;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private termsProvider: TermsProvider) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private termsProvider: TermsProvider, private alertCtrlProvider: AlertCtrlProvider) {
     }
 
-    onSearchInput(e) {
-        console.log('model: ', this.search);
-        console.log('onSearch: ', e);
+    async onSearchInput(e) {
+        await this.load();
+        this.terms = this.terms.filter(el => el.term.includes(this.search));
+        if (this.terms.length == 0) this.alertCtrlProvider.show('Aviso', 'Nenhum termo encontrado.');
     }
 
-    ionViewDidLoad() {
-        this.terms = this.termsProvider.getAll();
+    async load() {
+        return new Promise((resolve, reject) => {
+            this.terms$ = this.termsProvider.getAll();
+            this.terms$.subscribe((arr_terms: Term[]) => {
+                this.terms = arr_terms;
+                resolve();
+            });
+        });
+    }
+
+    async ionViewDidLoad() {
+        await this.load();
     }
 
 }
